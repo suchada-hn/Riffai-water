79e24f2455455f23d6731c644e36f7c12e458cf8
Suchada Hnoonpakdee <suchada.hnpd@gmail.com>
2026-03-25 20:58:29 +0700

    add chakjra

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -21,32 +21,32 @@ export default function RootLayout({
     <html lang="th">
       <body className="font-sans">
         <ChakraProvider theme={theme}>
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            duration: 3000,
-            style: {
-              background: "#212529",
-              color: "#ffffff",
-              border: "1px solid #343a40",
-              borderRadius: "2px",
-              fontWeight: "500",
-            },
-            success: {
-              iconTheme: {
-                primary: "#212529",
-                secondary: "#ffffff",
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              duration: 3000,
+              style: {
+                background: "#212529",
+                color: "#ffffff",
+                border: "1px solid #343a40",
+                borderRadius: "2px",
+                fontWeight: "500",
               },
-            },
-            error: {
-              iconTheme: {
-                primary: "#212529",
-                secondary: "#ffffff",
+              success: {
+                iconTheme: {
+                  primary: "#212529",
+                  secondary: "#ffffff",
+                },
               },
-            },
-          }}
-        />
-        <AlertCenter />
+              error: {
+                iconTheme: {
+                  primary: "#212529",
+                  secondary: "#ffffff",
+                },
+              },
+            }}
+          />
+          <AlertCenter />
           {children}
         </ChakraProvider>
       </body>
