99f86b695f92733d678054551e636cd9f5c4261b
Suchada Hnoonpakdee <suchada.hnpd@gmail.com>
2026-03-27 12:29:26 +0700

    update nav

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -35,15 +35,13 @@ export default function Navbar() {
             </div>
             <div>
               <svg
+                className={"h-12 w-24"}
                 xmlns="http://www.w3.org/2000/svg"
                 version="1.0"
-                width="308.000000pt"
-                height="218.000000pt"
                 viewBox="0 0 308.000000 218.000000"
-                preserveAspectRatio="xMidYMid meet"
               >
                 <g
-                  transform="translate(0.000000,218.000000) scale(0.100000,-0.100000)"
+                  transform="translate(0.000000,218.000000) scale(.100000,-0.100000)"
                   fill={"#000000"}
                   stroke="none"
                 >
